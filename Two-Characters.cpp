845677ddb4e1f0c4f5d2681678ba1dbfdf1d8e78
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-09-15 01:31:59 +0900

    Update Two-Characters.cpp

--- Two-Characters.cpp
+++ Two-Characters.cpp
@@ -1,1 +1,34 @@
+int alternate(string s)
+{
+    vector<int> vec;
+    
+    for (int i = 0; i < s.size()-1; ++i)
+    {
+        for (int j = i + 1; j < s.size(); ++j)
+        {
+            std::string str{};
+            for (int k = 0; k < s.size(); ++k)
+            {
+                if(s[k] == s[i] || s[k] == s[j])
+                {
+                    str += s[k];
+                }
+            }
+            
+            bool check = true;
+            for (int k = 0; k < str.size()-1; ++k)
+            {
+                if (str[k] == str[k+1])
+                {
+                    check = false;
+                    break;
+                }
+            }
+            
+            if (check) vec.push_back(str.size());
+        }
+    }
+    
+    return vec.empty() ? 0 : *std::max_element(vec.begin(),vec.end());
+}
 
