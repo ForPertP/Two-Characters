d732ce169972ceb3324e8870c1802e1b5bb591a6
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-01-30 01:38:45 +0900

    Update Two-Characters.cpp

--- Two-Characters.cpp
+++ Two-Characters.cpp
@@ -1,3 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'alternate' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
 int alternate(string s)
 {
     vector<int> vec;
