6488f9700d4411194aaebf218378b1266b87763e
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-01-31 22:24:59 +0900

    Update Two-Characters.cpp

--- Two-Characters.cpp
+++ Two-Characters.cpp
@@ -88,7 +88,18 @@ string ltrim2(const string &str) {
     return s;
 }
 
+
 string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(), s.end()
+    );
+
+    return s;
+}
+
+string rtrim2(const string &str) {
     string s(str);
 
     s.erase(
