b3761da3c556faa42c7e2c37d6a46483fc9db805
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-02-03 01:33:28 +0900

    Update Two-Characters.cpp

--- Two-Characters.cpp
+++ Two-Characters.cpp
@@ -52,7 +52,7 @@ int alternate2(string s)
 {
     vector<int> vec;
     
-    for (int i = 0; i < s.size() - 1; ++i)
+    for (int i = 0; i < s.size()-1; ++i)
     {
         for (int j = i + 1; j < s.size(); ++j)
         {
@@ -66,7 +66,7 @@ int alternate2(string s)
             }
             
             bool check = true;
-            for (int k = 0; k < str.size() - 1; ++k)
+            for (int k = 0; k < str.size()-1; ++k)
             {
                 if (str[k] == str[k+1])
                 {
