7caad4b744c610965f0d3785484f1934fae8a45e
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-02-04 03:01:22 +0900

    Update Two-Characters.cs

--- Two-Characters.cs
+++ Two-Characters.cs
@@ -1,1 +1,19 @@
-Two-Characters
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int l = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string s = Console.ReadLine();
+
+        int result = Result.alternate(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
