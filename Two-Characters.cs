850974326a04071c37cc146b590c961e558f711a
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-02-05 03:38:58 +0900

    Update Two-Characters.cs

--- Two-Characters.cs
+++ Two-Characters.cs
@@ -1,3 +1,28 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'alternate' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+}
 
 class Solution
 {
