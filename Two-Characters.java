b20c4ce522152d6b6848e42ad432489b9e41cd7d
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-02-08 00:10:24 +0900

    Update Two-Characters.java

--- Two-Characters.java
+++ Two-Characters.java
@@ -1,1 +1,19 @@
-Two-Characters
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int l = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String s = bufferedReader.readLine();
+
+        int result = Result.alternate(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
